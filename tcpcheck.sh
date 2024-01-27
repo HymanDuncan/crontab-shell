f43020f49d35ed13ac8e4312a32f79b673723768
Hyman Duncan <152063277+HymanDuncan@users.noreply.github.com>
2024-01-27 10:40:21 +0800

    Update tcpcheck.sh

--- tcpcheck.sh
+++ tcpcheck.sh
@@ -2,16 +2,16 @@
 if ! ps -ef | grep "tcpcopy" | grep -v "grep" > /dev/null;
          then
         /usr/local/tcpcopy/sbin/tcpcopy -l /var/log/tcpcopy.log -x 11111-192.168.8.11:11112 -s 192.168.8.11 -c 192.168.244.44 &
-    echo "tcpcopy started!"
+    echo "tcpcopy started! $(date +%T)"
 else
-    echo "tcpcopy already started!"
+    echo "tcpcopy already started! $(date +%T)"
 fi
 while true;
     do
     if ! ps -ef | grep "tcpcopy" | grep -v "grep" > /dev/null;
     then
         /usr/local/tcpcopy/sbin/tcpcopy -l /var/log/tcpcopy.log -x 11111-192.168.8.11:11112 -s 192.168.8.11 -c 192.168.244.44 &
-    echo "tcpcopy restart success!"
+    echo "tcpcopy restart success! $(date +%T)"
     fi
     sleep 10
 done
